26a31d46ec7126e28e8a702dbdc242f98e2c35fd
arvindshekar <arvindrocks07@gmail.com>
2017-06-17 17:17:01 -0500

    Initial commit

---
+++ src/PID.cpp
@@ -0,0 +1,43 @@
+#include "PID.h"
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {
+
+}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    p_error = 0.0;
+    d_error = 0.0;
+    i_error = 0.0;
+
+}
+
+
+void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error +=cte;
+}
+
+
+//}
+
+double PID::TotalError() {
+//negetive abecause of the steering angle
+    return - Kp * p_error - Ki * i_error - Kd * d_error;
+}
+
